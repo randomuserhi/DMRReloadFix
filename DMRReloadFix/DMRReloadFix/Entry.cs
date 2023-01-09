63a2e93c8af81b84cb8deca28fbe0a766b4d40f3
randomuserhi <magicalworld.nano@gmail.com>
2023-01-09 15:51:42 +0000

    Initial Commit

---
+++ DMRReloadFix/DMRReloadFix/Entry.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+using API;
+using KillIndicatorFix.Patches;
+using Player;
+
+namespace KillIndicatorFix
+{
+    public static class Module
+    {
+        public const string GUID = "randomuserhi.DMRReloadFix";
+        public const string Name = "DMRReloadFix";
+        public const string Version = "0.0.1";
+    }
+
+    [BepInPlugin(Module.GUID, Module.Name, Module.Version)]
+    internal class Entry : BasePlugin
+    {
+        public override void Load()
+        {
+            APILogger.Debug(Module.Name, "Loaded DMRReloadFix");
+            harmony = new Harmony(Module.GUID);
+            harmony.PatchAll();
+
+            APILogger.Debug(Module.Name, "Debug is " + (ConfigManager.Debug ? "Enabled" : "Disabled"));
+        }
+
+        private Harmony harmony;
+    }
+}
---
+++ DMRReloadFix/DMRReloadFix/Patches/Reload.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using HarmonyLib;
+
+using API;
+
+// TODO:: Add config value for delay allowed between client hit marker and server
+
+namespace DMRReloadFix.Patches
+{
+    /*
+     * Tracks HP seperately rather than using client side __instance.Health since I'm unsure if messing with that will change client behaviour.
+     */
+
+    [HarmonyPatch]
+    internal static class Reload
+    {
+
+    }
+}
