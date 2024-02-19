9a0c62a6104e18295b23f8e2009c617747620e96
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-02-19 01:09:51 +0000

    chore: updated to just perform the reload twice

--- DMRReloadFix/DMRReloadFix/Entry.cs
+++ DMRReloadFix/DMRReloadFix/Entry.cs
@@ -1,27 +1,18 @@
-﻿using BepInEx;
-using BepInEx.Logging;
+﻿using API;
+using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
-using UnityEngine;
 
-using API;
-using DMRReloadFix.Patches;
-using Player;
-
-namespace DMRReloadFix
-{
-    public static class Module
-    {
+namespace DMRReloadFix {
+    public static class Module {
         public const string GUID = "randomuserhi.DMRReloadFix";
         public const string Name = "DMRReloadFix";
         public const string Version = "0.0.1";
     }
 
     [BepInPlugin(Module.GUID, Module.Name, Module.Version)]
-    internal class Entry : BasePlugin
-    {
-        public override void Load()
-        {
+    internal class Entry : BasePlugin {
+        public override void Load() {
             APILogger.Debug(Module.Name, "Loaded DMRReloadFix");
             harmony = new Harmony(Module.GUID);
             harmony.PatchAll();
--- DMRReloadFix/DMRReloadFix/Patches/Reload.cs
+++ DMRReloadFix/DMRReloadFix/Patches/Reload.cs
@@ -1,44 +1,20 @@
-﻿using UnityEngine;
-using HarmonyLib;
-
-using API;
+﻿using HarmonyLib;
 using Player;
 
-namespace DMRReloadFix.Patches
-{
+namespace DMRReloadFix.Patches {
     [HarmonyPatch]
-    internal static class Reload
-    {
-        // TODO:: Fix the issue of this being a static bool prefix making it difficult for other plugins to patch this function
-
-        // Exact same reload implementation from game just rounding up instead of rounding down.
+    internal static class Reload {
+        private static bool callOriginal = false;
         [HarmonyPatch(typeof(PlayerAmmoStorage), nameof(PlayerAmmoStorage.GetClipBulletsFromPack))]
         [HarmonyPrefix]
-        public static bool GetClipBulletsFromPack(PlayerAmmoStorage __instance, ref int __result, int currentClip, AmmoType ammoType)
-        {
-            InventorySlotAmmo inventorySlotAmmo = __instance.m_ammoStorage[(int)ammoType];
-            float costOfBullet = inventorySlotAmmo.CostOfBullet;
-            int num = inventorySlotAmmo.BulletClipSize - currentClip;
-            if (num < 1)
-            {
-                __result = currentClip;
+        private static bool Prefix_GetClipBulletsFromPack(PlayerAmmoStorage __instance, ref int __result, int currentClip, AmmoType ammoType) {
+            if (!callOriginal) {
+                callOriginal = true;
+                __result = __instance.GetClipBulletsFromPack(__instance.GetClipBulletsFromPack(currentClip, ammoType), ammoType);
+                callOriginal = false;
                 return false;
             }
-            float a = (float)num * inventorySlotAmmo.CostOfBullet;
-            float ammoInPack = inventorySlotAmmo.AmmoInPack;
-            int num2 = Mathf.RoundToInt(Mathf.Min(a, ammoInPack) / costOfBullet); // Changed from FloorToInt
-            int num3 = inventorySlotAmmo.BulletsInPack;
-            inventorySlotAmmo.AmmoInPack -= (float)num2 * costOfBullet;
-            inventorySlotAmmo.OnBulletsUpdateCallback?.Invoke(inventorySlotAmmo.BulletsInPack);
-            currentClip += Mathf.Min(num2, num3);
-            __instance.NeedsSync = true;
-            __instance.UpdateSlotAmmoUI(inventorySlotAmmo, currentClip);
-            if (ConfigManager.Debug)
-            {
-                APILogger.Debug(Module.Name, $"AmmoStorage.GetClipFromPack cost: {costOfBullet} from storage: {ammoInPack} ret : {num2} currentClip: {currentClip} inPack: {__instance.m_ammoStorage[(int)ammoType].AmmoInPack}");
-            }
-            __result =  currentClip;
-            return false;
+            return true;
         }
     }
 }
