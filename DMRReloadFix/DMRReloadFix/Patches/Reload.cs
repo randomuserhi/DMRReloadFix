4a5f8bbc44f653ac7f9f65004ac8fe07daa5c34c
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2025-06-17 06:06:26 +0100

    Update Reload.cs

--- DMRReloadFix/DMRReloadFix/Patches/Reload.cs
+++ DMRReloadFix/DMRReloadFix/Patches/Reload.cs
@@ -3,11 +3,11 @@ using Player;
 
 namespace DMRReloadFix.Patches {
     [HarmonyPatch]
-    internal static class Reload {
-        private static bool callOriginal = false;
+    public static class Reload {
+        public static bool callOriginal = false;
         [HarmonyPatch(typeof(PlayerAmmoStorage), nameof(PlayerAmmoStorage.GetClipBulletsFromPack))]
         [HarmonyPrefix]
-        private static bool Prefix_GetClipBulletsFromPack(PlayerAmmoStorage __instance, ref int __result, int currentClip, AmmoType ammoType) {
+        public static bool Prefix_GetClipBulletsFromPack(PlayerAmmoStorage __instance, ref int __result, int currentClip, AmmoType ammoType) {
             if (!callOriginal) {
                 callOriginal = true;
                 __result = __instance.GetClipBulletsFromPack(__instance.GetClipBulletsFromPack(currentClip, ammoType), ammoType);
